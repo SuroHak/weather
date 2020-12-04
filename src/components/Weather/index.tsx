141da9f5a4e77c1b2e00556a8e13152596945818
SuroHak96 <suren.hakobyan@picsart.com>
2020-12-04 22:57:52 +0400

    minor change (3)

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -8,17 +8,10 @@ import WeatherOptions from '../WeatherOptions';
 const Weather = (): JSX.Element => {
     const currentWeather = useMemoSelector(state => state.currentWeather);
 
-    const currentWeatherList = useMemo(() => {
-        if (!currentWeather) return null;
-
-        return currentWeather.list
-            .filter((item: any) => item.dt_txt.includes('12:00') || item.dt_txt.includes('18:00'))
-    }, [currentWeather]);
-
     return currentWeather ? (
         <div className='weather-main'>
             <WeatherOptions />
-            <WeatherList weathers={currentWeatherList} />
+            <WeatherList weathers={currentWeather.list} />
         </div>
     ) : <Loading />
 }
